6e395540b5710c8461892f8843c025bd829ac991
Libertad18 <arnetaddisu7@gmail.com>
2025-07-30 08:30:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.getElementById("continueBtn").addEventListener("click", () => {
-  // Simple delay simulating they joined (Telegram doesn't confirm join status on frontend)
-  alert("Thanks for joining! Redirecting...");
-  window.location.href = "https://result.neaea.gov.et";
-});
+
